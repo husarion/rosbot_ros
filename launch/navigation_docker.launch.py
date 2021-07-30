9aee5e3d91ecc855c998235ea727592c62cab113
Khasreto <adam-krawczyk@outlook.com>
2021-07-30 12:12:41 +0200

    add docker nav launch

---
+++ launch/navigation_docker.launch.py
@@ -0,0 +1,17 @@
+import launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    rosbot_description = get_package_share_directory('rosbot_description')
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([rosbot_description, '/launch/rosbot_navigation_pro.launch.py']),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([rosbot_description, '/launch/slam_toolbox.launch.py']),
+        ),
+    ])
