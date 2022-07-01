44c54db0b729bd47bb64e6bc4d102c45454d7525
TopRamen1 <nicolas.duc@op.pl>
2022-07-01 12:18:56 +0200

    Added rosbot_bringup pkg

---
+++ src/rosbot_bringup/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup()
+d['packages'] = ['rosbot_bringup']
+d['package_dir'] = {'': 'scripts'}
+
+setup(**d)
