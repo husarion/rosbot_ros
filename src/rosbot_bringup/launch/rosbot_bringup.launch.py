8bdb96283001fc8df0df20054c0a309167f1ab24
Jakub Delicat <jakub.delicat@husarion.com>
2022-09-07 14:47:32 +0200

    added rosbot_bringup

---
+++ src/rosbot_bringup/launch/rosbot_bringup.launch.py
@@ -0,0 +1,128 @@
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch.substitutions import Command,  PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+from launch.event_handlers import OnProcessExit
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch.actions import RegisterEventHandler
+import os
+
+
+def generate_launch_description():
+    rosbot_description = get_package_share_directory('rosbot_description')
+    rosbot_bringup = get_package_share_directory('rosbot_bringup')
+
+    rosbot_description_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    rosbot_description,
+                    "launch",
+                    "rosbot_description.launch.py",
+                ]
+            )
+        )
+    )
+
+    xacro_file = os.path.join(
+        rosbot_description, 'models', 'rosbot', 'rosbot.urdf.xacro')
+    ekf_config = os.path.join(
+        rosbot_bringup, 'config', 'ekf.yaml')
+
+    robot_description = {
+        'robot_description': Command([
+            'xacro --verbosity 0 ', xacro_file
+        ])
+    }
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("rosbot_bringup"),
+            "config",
+            "rosbot_controllers.yaml",
+        ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        remappings=[
+            ("/imu_sensor_node/imu", "/_imu/data_raw"),
+            ("~/motors_cmd", "/_motors_cmd"),
+            ("~/motors_response", "/_motors_response"),
+            ("/rosbot_base_controller/cmd_vel_unstamped", "/cmd_vel"),
+        ],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "rosbot_base_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay start of robot_controller after joint_state_broadcaster
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = (
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[robot_controller_spawner],
+            )
+        )
+    )
+
+    imu_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "imu_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay start of imu_broadcaster after robot_controller
+    # when spawning without delay ros2_control_node sometimes crashed
+    delay_imu_broadcaster_spawner_after_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=[imu_broadcaster_spawner],
+        )
+    )
+
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[ekf_config]
+    )
+
+    actions = [
+        rosbot_description_launch,
+        control_node,
+        robot_localization_node,
+        joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        delay_imu_broadcaster_spawner_after_robot_controller_spawner,
+    ]
+
+    return LaunchDescription(actions)
--- src/rosbot_description/launch/rosbot_description.launch.py
+++ src/rosbot_description/launch/rosbot_description.launch.py
@@ -13,8 +13,6 @@ def generate_launch_description():
     rosbot_description = get_package_share_directory('rosbot_description')
     xacro_file = os.path.join(
         rosbot_description, 'models', 'rosbot', 'rosbot.urdf.xacro')
-    ekf_config = os.path.join(
-        rosbot_description, 'config', 'ekf.yaml')
 
     robot_description = {
         'robot_description': Command([
@@ -22,97 +20,14 @@ def generate_launch_description():
         ])
     }
 
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare("rosbot_description"),
-            "config",
-            "rosbot_controllers.yaml",
-        ]
-    )
-
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
-        remappings=[
-            ("/imu_sensor_node/imu", "/_imu/data_raw"),
-            ("~/motors_cmd", "/_motors_cmd"),
-            ("~/motors_response", "/_motors_response"),
-            ("/rosbot_base_controller/cmd_vel_unstamped", "/cmd_vel"),
-        ],
-    )
-
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[robot_description]
     )
 
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
-    robot_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "rosbot_base_controller",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
-     # Delay start of robot_controller after joint_state_broadcaster
-    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = (
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[robot_controller_spawner],
-            )
-        )
-    )
-
-    imu_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "imu_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
-    # Delay start of imu_broadcaster after robot_controller
-    # when spawning without delay ros2_control_node sometimes crashed
-    delay_imu_broadcaster_spawner_after_robot_controller_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=robot_controller_spawner,
-            on_exit=[imu_broadcaster_spawner],
-        )
-    )
-
-    robot_localization_node = Node(
-       package='robot_localization',
-       executable='ekf_node',
-       name='ekf_filter_node',
-       output='screen',
-       parameters=[ekf_config]
-    )
-
-
     actions = [
-        control_node,
         robot_state_publisher_node,
-        robot_localization_node,
-        joint_state_broadcaster_spawner,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
-        delay_imu_broadcaster_spawner_after_robot_controller_spawner,
     ]
 
     return LaunchDescription(actions)
