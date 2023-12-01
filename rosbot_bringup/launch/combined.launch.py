bfc971149d4dfa99e318d11ea936f2c38e7e3fb6
dominikn <dominik.nwk@gmail.com>
2023-12-01 17:45:23 +0100

    handle ROS_LOCALHOST_ONLY=1

--- rosbot_bringup/launch/combined.launch.py
+++ rosbot_bringup/launch/combined.launch.py
@@ -1,36 +1,58 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
 from launch_ros.actions import Node
+import os
+
+def generate_microros_agent_node(context, *args, **kwargs):
+    serial_port = LaunchConfiguration('serial_port').perform(context)
+    serial_baudrate = LaunchConfiguration('serial_baudrate').perform(context)
+    localhost_only_fastrtps_profiles_file = LaunchConfiguration('localhost_only_fastrtps_profiles_file').perform(context)
+
+    # Check ROS_LOCALHOST_ONLY environment variable
+    if os.environ.get('ROS_LOCALHOST_ONLY') == '1':
+        # Set RMW_IMPLEMENTATION and FASTRTPS_DEFAULT_PROFILES_FILE
+        rmw_implementation = 'rmw_fastrtps_cpp'
+        return [
+            SetEnvironmentVariable(name='RMW_IMPLEMENTATION', value=rmw_implementation),
+            SetEnvironmentVariable(name='FASTRTPS_DEFAULT_PROFILES_FILE', value=localhost_only_fastrtps_profiles_file),
+            Node(
+                package='micro_ros_agent',
+                executable='micro_ros_agent',
+                arguments=['serial', '-D', serial_port, '-b', serial_baudrate],
+                output='screen'
+            )
+        ]
+    else:
+        return [
+            Node(
+                package='micro_ros_agent',
+                executable='micro_ros_agent',
+                arguments=['serial', '-D', serial_port, '-b', serial_baudrate],
+                output='screen'
+            )
+        ]
 
 def generate_launch_description():
-    # Declare launch arguments for the serial port and baud rate
     declare_serial_port_arg = DeclareLaunchArgument(
         'serial_port',
-        default_value='/dev/ttySERIAL',  # Default value, replace if needed
+        default_value='/dev/ttySERIAL',
         description='Serial port for micro-ROS agent'
     )
 
     declare_serial_baudrate_arg = DeclareLaunchArgument(
         'serial_baudrate',
-        default_value='576000',  # Default baud rate, replace if needed
+        default_value='576000',
         description='Baud rate for serial communication'
     )
 
-    # Retrieve launch configurations
-    serial_port = LaunchConfiguration('serial_port')
-    serial_baudrate = LaunchConfiguration('serial_baudrate')
-
-    # Node configuration for micro-ROS agent
-    microros_agent_node = Node(
-        package='micro_ros_agent',
-        executable='micro_ros_agent',
-        arguments=['serial', '-D', serial_port, '-b', serial_baudrate],
-        output='screen'
+    declare_localhost_only_fastrtps_profiles_file_arg = DeclareLaunchArgument(
+        'localhost_only_fastrtps_profiles_file',
+        default_value='/microros_locahost_only.xml',
+        description='Path to the Fast RTPS default profiles file'
     )
 
-    # Include bringup.launch.py
     bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/bringup.launch.py'])
     )
@@ -38,6 +60,9 @@ def generate_launch_description():
     return LaunchDescription([
         declare_serial_port_arg,
         declare_serial_baudrate_arg,
-        microros_agent_node,
+        declare_localhost_only_fastrtps_profiles_file_arg,
+        OpaqueFunction(function=generate_microros_agent_node),
         bringup_launch
     ])
+
+
