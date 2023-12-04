de714102e0f375cd87e4a0c89bc9635c6f5bbda8
dominikn <dominik.nwk@gmail.com>
2023-12-04 13:28:45 +0100

    small fix

--- sync_with_robot.sh
+++ sync_with_robot.sh
@@ -3,8 +3,8 @@
 # If your ROSbot's IP addr is 10.5.10.64 execute:
 # ./sync_with_rosbot.sh 10.5.10.64
 
-sshpass -p "husarion" rsync -vRr --delete ./ husarion@$1:/home/husarion/${PWD##*/} 
+sshpass -p "husarion" rsync -vRr --delete ./ husarion@$1:/home/husarion/${PWD##*/}
 
-while inotifywait -r -e modify,create,delete,move ./ ; do 
-    sshpass -p "husarion" rsync -vRr --delete ./ husarion@$1:/home/husarion/${PWD##*/} 
+while inotifywait -r -e modify,create,delete,move ./ ; do
+    sshpass -p "husarion" rsync -vRr --delete ./ husarion@$1:/home/husarion/${PWD##*/}
 done
