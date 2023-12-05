fa12c36048bfe78d3c49264111e45754cc8cdd24
dominikn <dominik.nwk@gmail.com>
2023-12-05 18:31:09 +0100

    place .xml file in share directory

--- rosbot_bringup/setup.py
+++ rosbot_bringup/setup.py
@@ -27,6 +27,7 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "config"), glob("config/*.xml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
