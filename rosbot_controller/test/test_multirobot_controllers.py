9c87d3837ae003664354bdf2e2816f523c512791
Jakub Delicat <jakub.delicat@husarion.com>
2024-01-03 10:24:24 +0100

    Revert "removed coltroller test for testing timeout"
    This reverts commit 50c90e5028450640f9b2ce78584a76bceb3e6989.

---
+++ rosbot_controller/test/test_multirobot_controllers.py
@@ -0,0 +1,84 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Intel Corporation. All Rights Reserved.
+# Copyright 2023 Husarion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch_pytest
+import pytest
+import rclpy
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from test_utils import ControllersTestNode
+
+robot_names = ["rosbot1", "rosbot2", "rosbot3", "rosbot4"]
+
+
+@launch_pytest.fixture
+def generate_test_description():
+    rosbot_controller = get_package_share_directory("rosbot_controller")
+    actions = []
+    for i in range(len(robot_names)):
+        controller_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution(
+                    [
+                        rosbot_controller,
+                        "launch",
+                        "controller.launch.py",
+                    ]
+                )
+            ),
+            launch_arguments={
+                "use_sim": "False",
+                "mecanum": "True",
+                "use_gpu": "False",
+                "namespace": robot_names[i],
+            }.items(),
+        )
+
+        actions.append(controller_launch)
+
+    return LaunchDescription(actions)
+
+
+@pytest.mark.launch(fixture=generate_test_description)
+def test_multirobot_controllers_startup_success():
+    for robot_name in robot_names:
+        rclpy.init()
+        try:
+            node = ControllersTestNode(f"test_{robot_name}_controllers", namespace=robot_name)
+            node.create_test_subscribers_and_publishers()
+            node.start_publishing_fake_hardware()
+
+            node.start_node_thread()
+            msgs_received_flag = node.joint_state_msg_event.wait(timeout=10.0)
+            assert msgs_received_flag, (
+                f"Expected JointStates message but it was not received. Check {robot_name}/"
+                "joint_state_broadcaster!"
+            )
+            msgs_received_flag = node.odom_msg_event.wait(timeout=10.0)
+            assert msgs_received_flag, (
+                f"Expected Odom message but it was not received. Check {robot_name}/"
+                "rosbot_base_controller!"
+            )
+            msgs_received_flag = node.imu_msg_event.wait(timeout=10.0)
+            assert (
+                msgs_received_flag
+            ), f"Expected Imu message but it was not received. Check {robot_name}/imu_broadcaster!"
+        finally:
+            rclpy.shutdown()
---
+++ rosbot_controller/test/test_utils.py
@@ -0,0 +1,93 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Husarion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+
+from threading import Event
+from threading import Thread
+
+from rclpy.node import Node
+
+from sensor_msgs.msg import JointState, Imu
+from nav_msgs.msg import Odometry
+
+
+class ControllersTestNode(Node):
+    ROSBOT_HARDWARE_PUBLISHERS_RATE = 10.0
+
+    __test__ = False
+
+    def __init__(self, name="test_node", namespace=None):
+        super().__init__(name, namespace=namespace)
+        self.joint_state_msg_event = Event()
+        self.odom_msg_event = Event()
+        self.imu_msg_event = Event()
+
+    def create_test_subscribers_and_publishers(self):
+        self.joint_state_sub = self.create_subscription(
+            JointState, "joint_states", self.joint_states_callback, 10
+        )
+
+        self.odom_sub = self.create_subscription(
+            Odometry, "rosbot_base_controller/odom", self.odometry_callback, 10
+        )
+
+        self.imu_sub = self.create_subscription(Imu, "imu_broadcaster/imu", self.imu_callback, 10)
+
+        # TODO: @delipl namespaces have not been implemented in microros yet
+        self.imu_publisher = self.create_publisher(Imu, "/_imu/data_raw", 10)
+
+        self.joint_states_publisher = self.create_publisher(JointState, "/_motors_response", 10)
+
+        self.timer = None
+
+    def start_node_thread(self):
+        self.ros_spin_thread = Thread(target=lambda node: rclpy.spin(node), args=(self,))
+        self.ros_spin_thread.start()
+
+    def joint_states_callback(self, data):
+        self.joint_state_msg_event.set()
+
+    def odometry_callback(self, data):
+        self.odom_msg_event.set()
+
+    def imu_callback(self, data):
+        self.imu_msg_event.set()
+
+    def start_publishing_fake_hardware(self):
+        self.timer = self.create_timer(
+            1.0 / self.ROSBOT_HARDWARE_PUBLISHERS_RATE,
+            self.publish_fake_hardware_messages,
+        )
+
+    # TODO: @delipl namespaces have not been implemented in microros yet
+    def publish_fake_hardware_messages(self):
+        imu_msg = Imu()
+        imu_msg.header.stamp = self.get_clock().now().to_msg()
+        imu_msg.header.frame_id = "imu_link"
+
+        joint_state_msg = JointState()
+        joint_state_msg.header.stamp = self.get_clock().now().to_msg()
+        joint_state_msg.name = [
+            "fl_wheel_joint",
+            "fr_wheel_joint",
+            "rl_wheel_joint",
+            "rr_wheel_joint",
+        ]
+        joint_state_msg.position = [0.0, 0.0, 0.0, 0.0]
+        joint_state_msg.velocity = [0.0, 0.0, 0.0, 0.0]
+
+        self.imu_publisher.publish(imu_msg)
+        self.joint_states_publisher.publish(joint_state_msg)
