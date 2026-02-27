bf58f4ac37fd41383b32989b92362e94d9e6e208
rafal-gorecki <rafal.gorecki@husarion.com>
2026-02-27 13:23:33 +0100

    0.18.5

--- rosbot_utils/setup.py
+++ rosbot_utils/setup.py
@@ -27,7 +27,7 @@ launch_files = glob("launch/*")
 
 setup(
     name=package_name,
-    version="0.18.0",
+    version="0.18.5",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
